c615fc1aa2a827cff10a02c7f0e7ab1e9df6c672
darko1002001 <darkogrozdanovski@gmail.com>
2013-05-17 15:12:42 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AndroidCourseWebService::Application.config.secret_token = 'e43ff149c80cccff35bce26b3f98bb3096d69159120824e8fd97aebe5004d283b5740ca70bc5054f1efb554cf1ec4da01e7220b307387d9b0b482c46b57e9223'
